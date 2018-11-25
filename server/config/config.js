c3955a1fd6e7fc963dad0d9440d37b025d34a627
Alfredo Alvarado Lepe <alfredo.alvarado@upaep.edu.mx>
2018-11-25 12:59:07 -0600

    Rest server - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,20 @@
-//==============
-//Puerto
-//==============
-
+// ==============
+// Puerto
+// ==============
 process.env.PORT = process.env.PORT || 3000;
+
+// ==============
+// Entorno
+// ==============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ==============
+// Base de datos
+// ==============
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:eHhiqXTTHJp2Tsb@ds145639.mlab.com:45639/cafe';
+}
+process.env.URLDB = urlDB;
