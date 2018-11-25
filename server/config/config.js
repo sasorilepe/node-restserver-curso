efa8ece169e91409a43170d71e1a24be6f8d3c9d
Alfredo Alvarado Lepe <alfredo.alvarado@upaep.edu.mx>
2018-11-25 13:20:25 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,6 +15,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:eHhiqXTTHJp2Tsb@ds145639.mlab.com:45639/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
